ed4c4142460dc5d7f3a1882091ba914ce6117461
VadimKhurs <3130021@gmail.com>
2024-06-17 01:25:10 +0300

    Library

--- src/Pages/Library.tsx
+++ src/Pages/Library.tsx
@@ -1,13 +1,16 @@
 import ExerciseCard from "@/components/ExerciseCard"
+import exercises from "@/dummy-exercises.json";
 
 function Library() {
     return (
       <section className="flex flex-col mt-20">
             <h2 className="text-5xl font-semibold mb-24 text-center">Library</h2>
-            <div className="flex mx-20" style={{columnGap: "100px", justifyContent: "center"}}>
-                <ExerciseCard/>
-                <ExerciseCard/>
-                <ExerciseCard/>
+            <div className="flex mx-20" style={{gap: "2rem", justifyContent: "center", display: "grid",gridTemplateColumns: "repeat(auto-fit, minmax(20rem, 1fr))"}}>
+                {
+                  exercises.map((exercise) => (
+                    <ExerciseCard {...exercise} />
+                  ))
+                }
             </div>
       </section>
     )
--- src/components/ExerciseCard.tsx
+++ src/components/ExerciseCard.tsx
@@ -1,9 +1,46 @@
-import squats from "@/assets/exercise/squats.jpg"
+interface Exercise {
+  id: string;
+  image: string;
+  title: string;
+  description: string;
+  calorieBurning: number;
+}
 
-function ExerciseCard() {
-  return (
-    <div  style={ {padding: "30px" , width: "500px", height: "600px", borderRadius: "15px", border: "3px solid #e5e7eb"}}>
-      <img ></img>
+function ExerciseCard({id, image, title, description, calorieBurning}: Exercise) {
+  let calorieScore = [];
+  const calorieScoreLimit = 5;
+  for (let i:number = 0; i < calorieScoreLimit; ++i) {
+    i < calorieBurning ? calorieScore.push("flex-1 bg-orange-400/80"): calorieScore.push("flex-1 bg-gray-500/20");
+  }
+  return ( 
+    <div key={id} >
+      <div style={{position: "relative"}}>
+        <div className="border-[0.5px] border-slate-700/90 rounded-xl p-2 transition ease-in-out block pb-4 transition ease-in-out duration-200  hover:outline outline-2 outline-blue-500 relative overflow-hidden group glass-effect-sm" style={{background: "linear-gradient(180deg,rgba(226,232,255,0),rgba(226,232,255,.066))", zIndex: "1", position:"relative", height: "400px"}}>
+        <a href="/Library">
+          <img src={image} width="640" height="360" className="w-full object-cover overflow-hidden rounded-lg aspect-[16/9]" style={{color: "transparent"}}/>
+          <div className="mt-3 p-2">
+            <div className="flex items-center justify-between">
+              <div>
+                <div className="text-[0.6rem] font-medium text-gray-400 mb-1">Calorie burning</div>
+                <div className="flex w-40 h-1 rounded-lg overflow-hidden gap-[0.15rem]">
+                  {
+                    calorieScore.map((el) => (
+                      <div className={el}/>
+                    ))
+                  }
+                </div>
+              </div>
+            </div>
+            <div className="font-semibold text-black-300/80 mt-3">{title}</div>
+            <div className="text-gray-400/80 text-sm mt-1 font-normal">
+              <div className="LinesEllipsis LinesEllipsis--clamped ">
+                {description}
+              </div>
+            </div>
+          </div>
+        </a>
+        </div>
+      </div>
     </div>
   )
 }
